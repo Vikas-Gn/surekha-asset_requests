b9e67736e03de355208b9dc57349ec550f5a7885
Vikas-Gn <vikasgn2000@gmail.com>
2025-07-08 13:28:05 +0530

    updated-asset

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
 const app = express();
-const port = 3044;
+const port = 3600;
 
 // PostgreSQL connection configuration
 const pool = new Pool({
@@ -169,5 +169,5 @@ app.delete('/api/requests/clear', async (req, res) => {
 
 // Start server
 app.listen(port, () => {
-    console.log(`Server running at http://35.154.187.99:${port}`);
+    console.log(`Server running at http://3.85.61.23:${port}`);
 });
